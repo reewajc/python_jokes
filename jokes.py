832687609269093d9cec4af7ff3704f014fdcc66
reewajc <37374398+reewajc@users.noreply.github.com>
2020-01-27 13:52:59 -0600

    Getting all jokes from api

--- jokes.py
+++ jokes.py
@@ -1,4 +1,9 @@
 import pyjokes
 
-joke = pyjokes.get_joke(language='en')
-print(joke)
+# joke = pyjokes.get_joke(language='en')
+# print(joke)
+
+jokes = pyjokes.get_jokes(language='en')
+
+for joke in jokes:
+    print(joke)
